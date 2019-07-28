1908e523c6e43e83124dba3a8fd4fdbca2ea7297
sarah <sarahkharraz@gmail.com>
2019-07-28 13:43:49 +0100

    Create new test to see if the route: can return a string

--- test.js
+++ test.js
@@ -19,3 +19,15 @@ test('Home route returns a status code of 200', (t) => {
       t.end()
     })
 })
+
+test('Home route', (t) => {
+  supertest(router)
+    .get('/')
+    .expect(200)
+    .expect('Content-type', /html/)
+    .end((err, res) => {
+      t.error(err)
+      t.equal(res.text, 'Hello', 'Response should contain \'Hello\'')
+      t.end()
+    })
+})

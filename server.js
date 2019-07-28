f878073627edfd3c6cf65e29d47f5812e3639406
sarah <sarahkharraz@gmail.com>
2019-07-28 12:53:50 +0100

    Create server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const hostname = process.env.HOSTNAME || 'localhost'
+const port = process.env.PORT || 4000
+
+
+http.createServer().listen(port, hostname, () => {
+  console.log(`The server is running at port http://${hostname} : ${port}`)
+})

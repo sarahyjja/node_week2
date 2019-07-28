e0cd45c7b144e26997edda94afa35124248f319f
sarah <sarahkharraz@gmail.com>
2019-07-28 13:12:15 +0100

    test 2:Home route returns a status code of 200 > fail because router isnt exported

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 const test = require('tape')
 const supertest = require('supertest')
+const router = require('./router')
 
 test('Initialise', (t) => {
   let num = 2
   t.equal(num, 2, 'Should return 2');
   t.end()
+})
+
+test('Home route returns a status code of 200', (t) => {
+  supertest(router)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err)
+      t.equal(res.statusCode, 200, 'Should return 200')
+      t.end()
+    })
 })
